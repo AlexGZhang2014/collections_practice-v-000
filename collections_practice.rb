a7c694c692921055d69f10769374751ee53cc3fc
Alex Zhang <alexgzhang92@gmail.com>
2018-05-27 20:58:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,3 +21,12 @@ end
 def reverse_array(array)
   array.reverse
 end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |word|
+    word[2] = "$"
+    new_array.push(word)
+  end
+  new_array
+end
